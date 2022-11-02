389ae48abe5c55685afb602b283ee08b33465b37
Melissa <lynch.melissat@gmail.com>
2022-11-02 17:01:58 -0400

    reduxify folder component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import './App.css';
-//import ApiData from './components/ApiDataComponent';
+import FolderComponent from './components/FolderComponent';
 import ReduxComponent from './components/ReduxComponent'
 
 function App() {
   return (
     <div className="App">
-      <ReduxComponent />
+      <FolderComponent />
     </div>
   );
 }
--- src/components/FolderComponent.js
+++ src/components/FolderComponent.js
@@ -1,47 +1,70 @@
 import { Component } from 'react'
+import { connect } from 'react-redux'
+import { getUsersThunk } from '../store/getUsers'
+
 
 class FolderComponent extends Component {
-    constructor(props) {
-      super(props);
-      this.state = {
-        showlist: true 
-      }
-    }
-  
-    handleToggleClick = () => {
-      this.setState(state => {
-        return {showlist: !state.showlist}
-      });
-    }
-  
-    render() {
-      if(this.state.showlist) {
-        return (
-  
-          <div className="folder-container">
-            <h1>Home</h1>
-            <button id="toggle-button" onClick={this.handleToggleClick}>Toggle</button>
-            <ul>
-             <li>File 1</li>
-             <li>File 2</li>
-             <li>File 3</li>
-             <li>Hello from props: {this.props.data}</li>
-            </ul>
-          </div>
-  
-        );
-      } else {
-          return (
-  
-            <div className="folder-container">
-              <h1>Home</h1>
-              <button id="toggle-button" onClick={this.handleToggleClick}>Toggle</button>
-            </div>
-  
-          );
-      }
+  constructor(props) {
+    super(props);
+    this.state = {
+      showlist: false 
     }
   }
 
+  componentDidMount() {
+    //get thunk from props
+    this.props.getUsersThunk();
+  } 
+  
+  handleToggleClick = () => {
+    this.setState(state => {
+      return {showlist: !state.showlist}
+    });
+  }
+  
+  render() {
+    if(this.state.showlist) {
+      let users = this.props.users;
+      return (
+        <>
+          <button id="toggle-button" onClick={this.handleToggleClick}>
+            Toggle
+          </button>   
+          <div style={{margin: '10px'}}>
+            { users.map( (user) => {
+                return (
+                  <div key={user.id}>
+                    <h3>Name: {user.name}</h3>
+                    <h4>Email: {user.email}</h4> 
+                  </div>
+                )
+              })
+            }
+          </div>
+        </>
+        
+      );
+    } else {
+        return (
+          <button id="toggle-button" onClick={this.handleToggleClick}>
+            Toggle
+          </button>
+        );
+    }
+  }
+};
+//REDUX SETUP
+//add thunks to props
+function mapDispatch(dispatch) {
+  return { 
+    getUsersThunk: () => dispatch(getUsersThunk()) 
+  }
+}
+//add redux state to props
+function mapState(state) {
+  return {
+    users: state.users
+  }
+}
 
-export default FolderComponent; 
+export default connect(mapState, mapDispatch)(FolderComponent);
